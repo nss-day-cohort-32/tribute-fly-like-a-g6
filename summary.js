bd10b7f6ef44e46b27c1c0f256f9c331fffbad93
Carly Bergthold <carlybergthold@gmail.com>
2019-04-09 14:48:20 -0500

    final push muthas

--- summary.js
+++ summary.js
@@ -1,40 +1,5 @@
 
-// var robinHead = document.createElement("img");
-// robinHead.src = '/images/Robin.png';
-
-// $( ".button" ).click(function() {
-
-// });
-
-
-// $('.robin-png').hide();
-
-// $( ".summary-image" ).hover(function() {
-//     $( '.robin-png' ).toggle();
-//   });
-
-// FIRST JQUERYYYYY
-// $(document).ready(function(){
-// 	randomlyMoveImage();
-// });
-
-// function randomlyMoveImage(){
-//     var width = $(document).width();
-//     var height = $(document).height();
-    
-//     var x = Math.floor((Math.random() * width) + 1);
-//     var y = Math.floor((Math.random() * height) + 1);
-//       $( ".robin-png" ).animate({
-//     left: x,
-//           top: y
-//   }, 0, function() {
-//     // Animation complete.
-//   });
-
-//     setTimeout(randomlyMoveImage, 2500);
-// }
-
-// // SECOND JQUERYYYYY
+// MAKES ROBIN HEADS APPEAR
 $( ".button" ).click(function() {
     $('.summary-portrait').ready(function(){
         randomlyMoveImage();
